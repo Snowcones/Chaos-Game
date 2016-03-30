cd9546c147bd7f9d08caa9d2e5a6361a4c6b2a6d
Snowcones <Wchenningwchenning@gmail.com>
2016-03-30 02:52:47 -0400

    Added genetic algorithm code

--- Chaos Game/Form1.cs
+++ Chaos Game/Form1.cs
@@ -8,16 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        const int width = 1440;
-        const int height = 900;
+        const int width = 2880;
+        const int height = 1800;
 
         Random rand = new Random();
-        int[,] visitedPixels = new int [ width,height];
-        Bitmap bmp = new Bitmap(1440, 900);
+        int[,] visitedPixels = new int [width,height];
+        Bitmap bmp = new Bitmap(width, height);
 
         //Takes a list of functions, with the same form as runGame's functions, and a list of cumulative probabilities for the functions and iterates with them
         int runProbGame(double[] initialPosition, double[][] inFunctions, double[] probs, int[,] visited, int iter, double[] window)
@@ -27,8 +28,8 @@ namespace WindowsFormsApplication1
             double minY = window[2];
             double maxY = window[3];
             int numVisited = 0;
-            double averageDist = 0.0;
-            double[] pos = initialPosition;
+            //double averageDist = 0.0;
+            double[] pos = new double[] { initialPosition[0], initialPosition[1]};
             double[] temp = {0, 0};
             double[] function= {0, 0, 0, 0, 0,0};
             int x = 0, y = 0;
@@ -68,24 +69,25 @@ namespace WindowsFormsApplication1
                     if(visited[x, y] == 0)
                     {
                         numVisited++;
+                        visited[x, y]++;
                     }
 
-                    visited[x, y]++;
+                    
                 }
 
-                double dist = Math.Pow(pos[0] * pos[0] + pos[1] * pos[1], .5);
-                averageDist = averageDist*(i)/(i+1)+dist/(i+1);
+                //double dist = Math.Pow(pos[0] * pos[0] + pos[1] * pos[1], .5);
+                //averageDist = averageDist*(i)/(i+1)+dist/(i+1);
             }
             //Console.WriteLine(averageDist); //Uncomment this line to write the average distance of each point from the origin in the fractal
             return numVisited;
         }
 
-        int randomLinearFractal(int functionNumber, int iterations, int[,] store, double[] window) //Returns number of distinct pixels visited on the screen
+        //Returns number of distinct pixels visited in the window
+        int randomLinearFractal(int functionNumber, int iterations, int[,] store, double[] window) 
         {
             double[][] functions = new double[functionNumber][];
             double[] pos = { 0, 0 };
             double[] probs = new double[functionNumber];
-            double[] temp = { 0, 0 };
             for (int f = 0; f < functionNumber; f++)
             {
                 functions[f] = new double[6];
@@ -96,34 +98,13 @@ namespace WindowsFormsApplication1
             }
             for (int p = 0; p < functionNumber; p++)
             {
-                probs[p] = (1 + p) * 1.0 / functionNumber;
+                probs[p] = (1.0 + p) / functionNumber;
             }
             return runProbGame(pos, functions, probs, store, iterations, window);
         }
 
-        void runFern(int[,] dataStore, int iterations)
-        {
-            double[] window = { -8, 8, -2, 12 };
-            double[][] fern = new double[4][];              //For layout of these matricies see the comment above runGame
-            fern[0] = new double[6] { 0, 0, 0, .16, 0, 0 };
-            fern[1] = new double[6] { .85, .04, -.04, .85, 0, 1.60 };
-            fern[2] = new double[6] { .20, -.26, .23, .22, 0, 1.60 };
-            fern[3] = new double[6] { -.15, .28, .26, .24, 0, .44 };
-            double[] probs = { .01, .86, .93, 1.0 };
-            double[] initialPosition = { 0, 0 };
-
-            runProbGame(initialPosition, fern, probs, dataStore, iterations, window);
-        }
-
-        void runGalaxy(int galaxies, int points, int[,] store, double[] window)
-        {
-            for(int i=0; i<galaxies; i++)
-            {
-                randomLinearFractal(1, points, store, window);
-            }
-        }
-
-        void renderBinaryBitmap(int[,] dataSource, Bitmap drawingMap, Color offColor, Color onColor)    //Stores data in dataSource into the drawingMap Bitmap
+        //Writes pixelData from int[,] to a bitmap
+        void renderBinaryBitmap(int[,] dataSource, Bitmap drawingMap, Color offColor, Color onColor)    
         {
             for (int x = 0; x < dataSource.GetLength(0); x++)
             {
@@ -139,6 +120,52 @@ namespace WindowsFormsApplication1
                     }
                 }
             }
+        }
+        
+        double convertStringToDouble(String str, double min, double max)
+        {
+            UInt64 val = Convert.ToUInt64(str, 2);
+            double valMax = Math.Pow(2, str.Length) - 1.0;
+            return val / valMax * (max - min) + min;
+        }
+
+        void generateFunctionListFromGenome(Genetics.Genome inGenome, int numFunctions, int bitsPerVar, double[][] functions)
+        {
+            for (int f = 0; f < numFunctions; f++)
+            {
+                for (int v = 0; v < 6; v++)
+                {
+                    functions[f][v] = convertStringToDouble(inGenome.genes.Substring(bitsPerVar * (v + 6 * f), bitsPerVar), -1, 1);
+                }
+            }
+        }
+
+        Genetics.Genome[] reproduce(Genetics.Genome[] inGenomes, int[] inPixelsHit, double dblRate, double mutRate)
+        {
+            Array.Sort(inPixelsHit, inGenomes);
+            Array.Reverse(inGenomes);
+            Array.Sort(inPixelsHit);
+
+            Genetics.Genome[] children = new Genetics.Genome[inGenomes.Length];
+
+            //Keep 20% of the best fractals
+            for (int i = 0; i < (int)(children.Length * .2); i++)
+            {
+                children[i] = inGenomes[i];
+            }
+
+            //For the other 80%, choose two of the best 20% and make a child
+            for (int i = (int)(children.Length * .2); i < children.Length; i++)
+            {
+                //Randomly choose parents from the top 20%
+                int randFatherIndex = rand.Next(0, (int)(children.Length * .2));
+                int randMotherIndex = rand.Next(0, (int)(children.Length * .2));
+
+                //Create a genome for the child from the genomes of the parents
+                Genetics.Genome child = inGenomes[randFatherIndex].offspring(inGenomes[randMotherIndex], dblRate, mutRate);
+                children[i] = child;
+            }
+            return children;
         }
 
         public Form1()
@@ -157,12 +184,95 @@ namespace WindowsFormsApplication1
             ResizeRedraw = true;
             DoubleBuffered = true;
 
+            double[] initialPosition = { 0, 0 };
             double[] standardWindow = { -2, 2, -2, 2 };
-            //runFern(visitedPixels, 1000000);
-            //runGalaxy(300, 10000, visitedPixels, standardWindow);
-            randomLinearFractal(2, 100000, visitedPixels, standardWindow);
 
-            renderBinaryBitmap(visitedPixels, bmp, Color.Black, Color.White);
+            const int numFunctions = 10;                 //Number of functions in each fractal
+            const int fractalPoolSize = 80;             //Number of fractals generated in each generation
+            const double dblRate = .5;
+            const double mutRate = .02;                 //Mutation rate during evolution
+            const int precisionOfGenomeVariables = 16;  //Bits of precision for the variables in our genomes
+
+            //*************************************************************
+            //Lower pixelsToPlot and goalPixelsOnScreen to speed up program
+            //*************************************************************
+
+            const int goalPixelsOnScreen = 50000;       //Fractal evolution will proceed for this many generations
+            const int pixelsToPlot = 100000;            //or until a fractal has goalPixels in the window after pixelsToPlot iterations
+            const int generations = 50;                 //
+
+            int[] pixelsHit = new int[fractalPoolSize];
+            double[][] functions = new double[numFunctions][];
+            double[] probs = new double[numFunctions];
+            Genetics.Genome[] genomes = new Genetics.Genome[fractalPoolSize];
+            int numVarsInGenome = 6 * numFunctions; //Number of variables in each genome
+            
+            for (int i=0; i<numFunctions; i++)      //Sets up cumulative probabilities array for the fractals
+            {
+                probs[i] = (i + 1.0) / numFunctions;
+            }
+            for(int i=0; i<fractalPoolSize; i++)    //Initialize the fractals' genomes randomly
+            {
+                genomes[i] = new Genetics.Genome(numVarsInGenome * precisionOfGenomeVariables);
+            }
+            for (int i = 0; i < numFunctions; i++)  //Initialize functions to be empty
+            {
+                functions[i] = new double[6];
+            }
+
+            
+            int maxPixels = 0;      //This max is used to decide when to render a new bitmap
+            int maxPixelsSeen = 0;  //This max is used to log information, it is not used in any calculations
+            for(int gen=1; gen<generations+1 && maxPixels< goalPixelsOnScreen; gen++)
+            {
+                for (int i = 0; i < genomes.Length; i++)
+                {
+                    Array.Clear(visitedPixels, 0, visitedPixels.Length);
+                    generateFunctionListFromGenome(genomes[i], numFunctions, precisionOfGenomeVariables, functions);
+                    pixelsHit[i] = runProbGame(initialPosition, functions, probs, visitedPixels, pixelsToPlot, standardWindow);
+                    if ((pixelsHit[i] > maxPixels && gen == generations) || pixelsHit[i] >= goalPixelsOnScreen)
+                    {
+                        maxPixels = pixelsHit[i];
+                        Console.WriteLine(pixelsHit[i]);
+                        renderBinaryBitmap(visitedPixels, bmp, Color.Black, Color.White);
+                    }
+                    if (pixelsHit[i] > maxPixelsSeen)
+                    {
+                        maxPixelsSeen = pixelsHit[i];
+                    }
+                    if (pixelsHit[i] >= goalPixelsOnScreen)
+                    {
+                        Console.WriteLine("Found satisfactory fractal on generation: {0:d}, fractal number: {1:d} of the generation", gen, i+1);
+                        break;
+                    }
+                }
+                Console.WriteLine("{0:d}, {1:d}/{2:d}", gen, maxPixelsSeen, goalPixelsOnScreen);
+                genomes=reproduce(genomes, pixelsHit, dblRate, mutRate);
+            }
+        }
+
+        //Draws the barnsley fern
+        void runFern(int[,] dataStore, int iterations)
+        {
+            double[] window = { -8, 8, -2, 12 };
+            double[][] fern = new double[4][];              //For layout of these matricies see the comment above runGame
+            fern[0] = new double[6] { 0, 0, 0, .16, 0, 0 };
+            fern[1] = new double[6] { .85, .04, -.04, .85, 0, 1.60 };
+            fern[2] = new double[6] { .20, -.26, .23, .22, 0, 1.60 };
+            fern[3] = new double[6] { -.15, .28, .26, .24, 0, .44 };
+            double[] probs = { .01, .86, .93, 1.0 };
+            double[] initialPosition = { 0, 0 };
+
+            runProbGame(initialPosition, fern, probs, dataStore, iterations, window);
+        }
+
+        //Draws a bunch of fractals that look like galaxies
+        void runGalaxy(int galaxies, int points, int[,] store, double[] window)
+        {
+            for (int i = 0; i < galaxies; i++)
+            {
+                randomLinearFractal(1, points, store, window);
+            }
         }
     }
 }
