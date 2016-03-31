7e0291730a6eb2a5bea0896e2ad325e5e3ae9545
dangeng <dangengdg@gmail.com>
2016-03-30 21:03:20 -0400

    Update Form1.cs
    Made runProbGame take fewer arguements
    added a "coordinate" struct to handle 'pos' and 'temp'
    turned the functions into a list
    rewrote probabilities so all functions are equally probable
    deleted "renderFern" and "renderGalaxy"
    added a logged density renderer in addition to the binary renderer

--- Chaos Game/Form1.cs
+++ Chaos Game/Form1.cs
@@ -16,111 +16,64 @@ namespace WindowsFormsApplication1
         const int height = 900;
 
         Random rand = new Random();
-        int[,] visitedPixels = new int [ width,height];
         Bitmap bmp = new Bitmap(1440, 900);
 
+        double[] window = { -2, 2, -2, 2 };
+
+        int[,] visitedPixels = new int[width, height];
+
         //Takes a list of functions, with the same form as runGame's functions, and a list of cumulative probabilities for the functions and iterates with them
-        int runProbGame(double[] initialPosition, double[][] inFunctions, double[] probs, int[,] visited, int iter, double[] window)
+        void runProbGame(List<double[]> functions, int iter)
         {
             double minX = window[0];
             double maxX = window[1];
             double minY = window[2];
             double maxY = window[3];
-            int numVisited = 0;
-            double averageDist = 0.0;
-            double[] pos = initialPosition;
-            double[] temp = {0, 0};
-            double[] function= {0, 0, 0, 0, 0,0};
+
+            Coordinates pos = new Coordinates(0, 0);
+            Coordinates temp = new Coordinates(0, 0);
+            
             int x = 0, y = 0;
-            for(int i=0; i< iter; i++)
+
+            for (int i = 0; i < iter; i++)
             {
-                //Here we get a function from the list
-                double p = rand.NextDouble();
-                if(p<=probs[0])
-                {
-                    function = inFunctions[0];
-                }
-                else
-                {
-                    for(int funcTest=1; funcTest<probs.Length; funcTest++)
-                    {
-                        if(p<probs[funcTest] && p>probs[funcTest-1])
-                        {
-                            function = inFunctions[funcTest];
-                            break;
-                        }
-                    }
-                }
+                int p = rand.Next(functions.Count);   //Choose random function
 
-                temp[0] = pos[0] * function[0] + pos[1] * function[1] + function[4];
-                temp[1] = pos[0] * function[2] + pos[1] * function[3] + function[5];
-                pos[0] = temp[0];
-                pos[1] = temp[1];
+                //Perform affine transform
+                temp.x = pos.x * functions.ElementAt(p)[0] + pos.y * functions.ElementAt(p)[1] + functions.ElementAt(p)[4];
+                temp.y = pos.x * functions.ElementAt(p)[2] + pos.y * functions.ElementAt(p)[3] + functions.ElementAt(p)[5];
+                
+                pos.x = temp.x;     //Store transformation
+                pos.y = temp.y;
 
-                if (pos[0] <= maxX && pos[0] >= minX && pos[1] <= maxY && pos[1] >= minY)
+                if (pos.x <= maxX && pos.x >= minX && pos.y <= maxY && pos.y >= minY)
                 {
-                    double normalizedX = (pos[0] - minX) / (maxX - minX);
+                    double normalizedX = (pos.x - minX) / (maxX - minX);
                     x = (int)(normalizedX * width);
 
-                    double normalizedY = (pos[1] - minY) / (maxY - minY);
+                    double normalizedY = (pos.y - minY) / (maxY - minY);
                     y = (int)(height - normalizedY * height);
-
-                    if(visited[x, y] == 0)
-                    {
-                        numVisited++;
-                    }
-
-                    visited[x, y]++;
                 }
 
-                double dist = Math.Pow(pos[0] * pos[0] + pos[1] * pos[1], .5);
-                averageDist = averageDist*(i)/(i+1)+dist/(i+1);
+                visitedPixels[x, y]++;
             }
-            //Console.WriteLine(averageDist); //Uncomment this line to write the average distance of each point from the origin in the fractal
-            return numVisited;
         }
 
-        int randomLinearFractal(int functionNumber, int iterations, int[,] store, double[] window) //Returns number of distinct pixels visited on the screen
+        void randomLinearFractal(int functionNumber, int iterations) //Returns number of distinct pixels visited on the screen
         {
-            double[][] functions = new double[functionNumber][];
-            double[] pos = { 0, 0 };
-            double[] probs = new double[functionNumber];
-            double[] temp = { 0, 0 };
+            List <double[]> functions = new List<double[]>();
+
             for (int f = 0; f < functionNumber; f++)
             {
-                functions[f] = new double[6];
+                functions.Add(new double[6]);
+
                 for (int c = 0; c < 6; c++)
                 {
-                    functions[f][c] = 2*rand.NextDouble()-1.0;
+                    functions.ElementAt(f)[c] = 2 * rand.NextDouble() - 1.0;
                 }
             }
-            for (int p = 0; p < functionNumber; p++)
-            {
-                probs[p] = (1 + p) * 1.0 / functionNumber;
-            }
-            return runProbGame(pos, functions, probs, store, iterations, window);
-        }
 
-        void runFern(int[,] dataStore, int iterations)
-        {
-            double[] window = { -8, 8, -2, 12 };
-            double[][] fern = new double[4][];              //For layout of these matricies see the comment above runGame
-            fern[0] = new double[6] { 0, 0, 0, .16, 0, 0 };
-            fern[1] = new double[6] { .85, .04, -.04, .85, 0, 1.60 };
-            fern[2] = new double[6] { .20, -.26, .23, .22, 0, 1.60 };
-            fern[3] = new double[6] { -.15, .28, .26, .24, 0, .44 };
-            double[] probs = { .01, .86, .93, 1.0 };
-            double[] initialPosition = { 0, 0 };
-
-            runProbGame(initialPosition, fern, probs, dataStore, iterations, window);
-        }
-
-        void runGalaxy(int galaxies, int points, int[,] store, double[] window)
-        {
-            for(int i=0; i<galaxies; i++)
-            {
-                randomLinearFractal(1, points, store, window);
-            }
+            runProbGame(functions, iterations);
         }
 
         void renderBinaryBitmap(int[,] dataSource, Bitmap drawingMap, Color offColor, Color onColor)    //Stores data in dataSource into the drawingMap Bitmap
@@ -131,11 +84,54 @@ namespace WindowsFormsApplication1
                 {
                     if (dataSource[x, y] == 0)
                     {
-                        drawingMap.SetPixel(x, y, offColor);
+                        drawingMap.SetPixel(x, y, Color.Black);
                     }
                     else
                     {
-                        drawingMap.SetPixel(x, y, onColor);
+                        drawingMap.SetPixel(x, y, Color.White);
+                    }
+                }
+            }
+        }
+
+        void renderLoggedBitmap(int[,] dataSource, Bitmap drawingMap, Color offColor, Color onColor)
+        {
+            int[] flatData = dataSource.Cast<int>().ToArray();
+
+            double[,] loggedData = new double[dataSource.GetLength(0), dataSource.GetLength(1)];
+
+            for (int x = 0; x < dataSource.GetLength(0); x++)
+            {
+                for (int y = 0; y < dataSource.GetLength(1); y++)
+                {
+                    if (dataSource[x, y] != 0)
+                    {
+                        loggedData[x, y] = Math.Log(Convert.ToDouble(dataSource[x, y]), 2);
+                    }
+                    else
+                    {
+                        loggedData[x, y] = 0;
+                    }
+                }
+            }
+
+            double max = loggedData.Cast<double>().Max();
+
+            double[] flat = loggedData.Cast<double>().ToArray();
+
+            for (int x = 0; x < loggedData.GetLength(0); x++)
+            {
+                for (int y = 0; y < loggedData.GetLength(1); y++)
+                {
+                    int greyscale = (int)(255 * loggedData[x, y] / max);
+
+                    if (loggedData[x, y] == 0)
+                    {
+                        drawingMap.SetPixel(x, y, Color.FromArgb(greyscale, greyscale, greyscale));
+                    }
+                    else
+                    {
+                        drawingMap.SetPixel(x, y, Color.FromArgb(greyscale, greyscale, greyscale));
                     }
                 }
             }
@@ -156,13 +152,25 @@ namespace WindowsFormsApplication1
         {
             ResizeRedraw = true;
             DoubleBuffered = true;
+            
+            for (int i = 0; i < 1; i++)
+            {
+                randomLinearFractal(4, 1000000);
+            }
 
-            double[] standardWindow = { -2, 2, -2, 2 };
-            //runFern(visitedPixels, 1000000);
-            //runGalaxy(300, 10000, visitedPixels, standardWindow);
-            randomLinearFractal(2, 100000, visitedPixels, standardWindow);
+            renderLoggedBitmap(visitedPixels, bmp, Color.Black, Color.White);
+        }
+    }
 
-            renderBinaryBitmap(visitedPixels, bmp, Color.Black, Color.White);
+    struct Coordinates
+    {
+        public double x;
+        public double y;
+
+        public Coordinates(double xIn, double yIn)
+        {
+            x = xIn;
+            y = yIn;
         }
     }
 }
